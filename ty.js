49abea91a3a26e78207cefbebb0e0fd0c3e05069
ariawahyu <ariawahyupratama11@gmail.com>
2025-10-25 19:10:01 +0700

    Update ty.js

--- ty.js
+++ ty.js
@@ -7,9 +7,10 @@ return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' ')
 };
 
 document.getElementById('btn-home').addEventListener('click', function() {
-window.location.href = '/index.html'; 
+  window.location.href = 'index.html'; 
 });
 
 document.getElementById('btn-pinjam-lagi').addEventListener('click', function() {
-window.location.href = '/index2.html';
+  window.location.href = 'index2.html';
+
 });
